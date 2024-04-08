b70604c799fa255d40a0d877e221aea2d5bad4b2
Annie <annie.the.web.coder@gmail.com>
2024-04-08 17:24:41 +0000

    skeleton

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,15 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Task Manager App</h1>`)
+})
+
+// app.get('/api/v1/tasks')
+// app.post('/api/v1/tasks')
+// app.get('/api/v1/tasks/:id')
+// app.patch('/api/v1/tasks/:id')
+// app.delete('/api/v1/tasks/:id')
+
+const port = 3000
+app.listen(port, console.log(`Server listening on port ${port}...`))
