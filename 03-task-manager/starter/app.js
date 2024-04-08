1114dd28f944e7d5cd6a8c2eb56869ad4c500e5d
Annie <annie.the.web.coder@gmail.com>
2024-04-08 20:20:27 +0000

    refactoring db

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,10 +4,14 @@ const express = require('express')
 const app = express()
 const tasksRouter = require('./routes/tasks')
 const { getAllTasks, getTask } = require('./controllers/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
-// app.get('/', (req, res) => {
-//     res.send(`<h1>Task Manager App</h1>`)
-// })
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Task Manager App</h1>`)
+})
 
 app.use('/api/v1/tasks', tasksRouter)
 
@@ -18,4 +22,14 @@ app.use('/api/v1/tasks', tasksRouter)
 // app.delete('/api/v1/tasks/:id')
 
 const port = 3000
-app.listen(port, console.log(`Server listening on port ${port}...`))
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server listening on port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,14 +1,24 @@
 const mongoose = require('mongoose')
 
-const connectString = 'mongodb+srv://anniethewebcoder:KrERNzFxEzOha3qu@clusters.dbphh7w.mongodb.net/?retryWrites=true&w=majority&appName=Clusters'
+const connectDB = (url) => {
+    return mongoose.connect(url, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        })
+}
 
-mongoose.connect(connectString, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('CONNECTED TO THE DB')
-}).catch((err) => {
-    console.log(err)
-})
+module.exports = connectDB
+
+// mongoose.connect(connectString, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+// }).then(() => {
+//     console.log('CONNECTED TO THE DB')
+// }).catch((err) => {
+//     console.log(err)
+// })
+
