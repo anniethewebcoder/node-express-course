7b2bc6e4fe255747b2e9260eecd747d65977c343
Annie <annie.the.web.coder@gmail.com>
2024-04-08 20:50:25 +0000

    mongoose schema

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,14 +1,20 @@
+const Task = require('./../models/Task')
+
 const getAllTasks = (req, res) => {
     res.send('GET ALL TASKS')
 }
 
-const createTask = (req, res) => {
-    res.json(req.body)
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json({
+        task
+    })
+
 }
 
 const getTask = (req, res) => {
     res.json({
-        name: req.params
+        id: req.params.id
     })
 }
 
