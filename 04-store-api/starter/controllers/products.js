75e2947dcd29238f305022450ea16f484b071a62
anniethewebcoder <anniethewebcoder@gmail.com>
2024-05-01 17:05:35 -0400

    populate

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,13 +1,41 @@
+const Product = require('./../models/product')
+
 const getAllProductsStatic = async(req, res) => {
-    throw new Error('testing async errors')
+    const products = await Product.find({
+        name: 'vase table'
+    })
     res.status(200).json({
-        msg: 'product testing route'
+        products,
+        nbHits: products.length
     })
 }
 
 const getAllProducts = async(req, res) => {
+    const { 
+        featured, 
+        company,
+        name
+    } = req.query
+
+    const queryObject = {}
+
+    if(featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+
+    if(company) {
+        queryObject.company = company
+    }
+
+    if(name) {
+        queryObject.name = name
+    }
+
+    const products = await Product.find(queryObject)
+
     res.status(200).json({
-        msg: 'products route'
+        products,
+        nbHits: products.length
     })
 }
 
